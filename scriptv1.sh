07206cc8355e6cd72a29872b5c00db73fce5a26e
PoneyBleuv3 <150467147+PoneyBleuv3@users.noreply.github.com>
2024-05-03 22:22:06 +0200

    Update scriptv1.sh

--- scriptv1.sh
+++ scriptv1.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "Début du script"
+# Vérification des privilèges root
+if [ "$EUID" -ne 0 ]
+  then echo "Ce script doit être exécuté en tant que root"
+  exit
+fi
+
+# Mise à jour des paquets
+apt update
+
+# Notification de la fin de l'opération
+echo "La mise à jour des paquets est terminée."
